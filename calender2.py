c6e37221f3ada303a953b7f92677ab7d6a4fadf2
34dihydroxyphentylamine <102799272+34dihydroxyphentylamine@users.noreply.github.com>
2026-02-26 17:58:15 +0100

    Create calender2.py

---
+++ calender2.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+abschluss_text = input("Wann ist Dein Abschluss? ")
+abschluss_datum = datetime.strptime(abschluss_text,"%d,%m,%Y")
+heute = datetime.now()
+zeitspanne =  abschluss_datum - heute
+print(f"Bis Dein Abschluss sind {zeitspanne.days} geblieben")
+
+#Benutzer hat Flug am 28.02.2026 um 12.18, gebe aus, wie viel Zeit ist ihm geblieben
+
